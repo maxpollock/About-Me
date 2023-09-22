2026facaf1b32fa1224213afad47ec3314977e70
MAX POLLOCK <mcpollock00@gmail.com>
2023-09-22 16:16:41 +0100

    Update Js. Put quiz questions into individual functions.

--- app.js
+++ app.js
@@ -1,49 +1,66 @@
 "use strict";
-
+let correctAnswer = 0;
+let userAttempt = 0;
+let start = 0;
 let myUsername;
 
 function askName() {
   let myUsername = prompt("Hey, what's your name?");
   document.write(myUsername);
+  return myUsername;
 }
 
-// Please answer true or false to the following questions.
-// I have a black belt in a martial art.
-// I was awarded a Blue Peter badge as a child.
-// I had a cat called Ozzy, after Ozzy Osbourne of Black Sabbath.
-// I once gave Queen Elizabeth a high-five
-// I'm level 6 graded in Drums
-
-function quizQuestions() {
-  let correctAnswer = 0;
+function quizStart() {
   let start = confirm(
     "You've clicked to start the quiz. Click Ok to start or Cancel to exit."
   );
-  if (start === false) {
+  if (start === true) {
+    alert("Let's begin! Remember, yes or no answers only.");
+    myQuiz();
+  } else {
     alert("Quiz has been cancelled, you party pooper.");
     return;
-  } else {
-    alert("Let's begin! Remember, yes or no answers only.");
   }
+}
 
+function myQuiz() {
+  blackBeltQu();
+  bluePeterQu();
+  catQu();
+  queenQu();
+  drumsQu();
+  dogQu();
+  placesQu();
+  marksCheck();
+}
+
+function blackBeltQu() {
   let blackBelt = prompt("I have a black belt in a martial art.").toLowerCase();
   if (blackBelt === "no") {
     alert("Wrong! Everbody was Kung Fu fighting! (including me).");
-  } else {
-    correctAnswer++;
+  } else if (blackBelt !== "yes" || blackBelt !== "no") {
+    blackBelt = prompt("Sorry! Please answer with either 'yes' or 'no'.");
+  } else if (blackBelt === "yes") {
     alert("Correct, everbody was Kung Fu fighting! (including me).");
+    correctAnswer++;
   }
+}
 
+function bluePeterQu() {
   let bluePeter = prompt(
     "I was awarded a Blue Peter badge as a child."
   ).toLowerCase();
   if (bluePeter === "yes") {
     alert("I wish! My girlfriend got one for doing some drawings though.");
-  } else {
-    correctAnswer++;
+  } else if (bluePeter === "no") {
     alert("Yep, I haven't got one. I can still dream though, right?");
+    correctAnswer++;
+  } else if (bluePeter !== "yes" || bluePeter !== "no") {
+    bluePeter = prompt("Sorry! Please answer with either 'yes' or 'no'.");
   }
+}
 
+function catQu() {
   let ozzy = prompt(
     "I had a cat called Ozzy, after Ozzy Osbourne of Black Sabbath."
   ).toLowerCase();
@@ -51,44 +68,55 @@ function quizQuestions() {
     alert(
       "Incorrect! I had one called Chilly too, after Red Hot Chilli Peppers."
     );
+  } else if (ozzy !== "yes" || ozzy !== "no") {
+    ozzy = prompt("Sorry! Please answer with either 'yes' or 'no'.");
   } else {
     correctAnswer++;
     alert(
       "Nice one, I had another cat called Chilli too, named after the Red Hot Chilli Peppers."
     );
   }
+}
 
+function queenQu() {
   let queen = prompt("I once gave Queen Elizabeth a high-five.").toLowerCase();
   if (queen === "yes") {
     alert("I infact did not high five lizzie.");
+  } else if (queen !== "yes" || queen !== "no") {
+    queen = prompt("Sorry! Please answer with either 'yes' or 'no'.");
   } else {
     correctAnswer++;
     alert("Well done, of course I didn't.");
   }
+}
 
+function drumsQu() {
   let drums = prompt("I'm level 6 graded in Drums").toLowerCase();
   if (drums === "no") {
     alert("Bah dum tiss! I do have a grade 6 in Drums.");
+  } else if (drums !== "yes" || drums !== "no") {
+    drums = prompt("Sorry! Please answer with either 'yes' or 'no'.");
   } else {
     correctAnswer++;
     alert("Yup! I started learning when I was 7 years old.");
   }
+}
 
+function dogQu() {
   let ageQu = prompt(
     "Rounded up to a whole year, how old is my dog? Please answer with a number for this one."
   );
-  let userAttempt = 0;
 
   for (let i = 1; i < 5; i++) {
     userAttempt++;
     if (ageQu > 2) {
       ageQu = prompt("She's not that old, please try again with a lower age!");
     } else if (ageQu < 2) {
-      // userAttempt++;
       ageQu = prompt("Kor she's older than that! Please try again.");
     } else {
       correctAnswer++;
       alert("Nice one :) You got it right!");
+      userAttempt--;
       break;
     }
   }
@@ -96,7 +124,9 @@ function quizQuestions() {
   if (userAttempt > 4) {
     alert("Sorry you have ran out of attempts. She is 2 years old.");
   }
+}
 
+function placesQu() {
   let myPlaces = [
     "cambridge",
     "norwich",
@@ -126,29 +156,22 @@ function quizQuestions() {
   }
 
   if (answerAttempt > 5) {
-    alert(
-      "Ah unlucky! Your options were either: Norwich, Cambridge, Essex or Dubai"
-    );
+    alert(`Ah unlucky! Your options were either: ${myPlaces}`);
   }
+}
 
+function marksCheck() {
   if (correctAnswer > 6) {
     alert(
-      "Wow! Full marks! Thanks for taking part in my quiz. You got " +
-        correctAnswer +
-        " questions right! Smashed it :)"
+      `Wow! Full marks! Thanks for taking part in my quiz. You got ${correctAnswer} questions right! Smashed it :)`
     );
-    ``;
   } else if (correctAnswer > 3) {
     alert(
-      "Nice one, you didn't do too bad. Thanks for taking part in my quiz. You got " +
-        correctAnswer +
-        " questions correct :)"
+      `Nice one, you didn't do too bad. Thanks for taking part in my quiz. You got ${correctAnswer} questions correct :)`
     );
   } else {
     alert(
-      "Unlucky. You got " +
-        correctAnswer +
-        " questions correct. They do say it's the participation that counts :)"
+      `Unlucky. You got ${correctAnswer} questions correct. They do say it's the participation that counts :)`
     );
   }
 }
